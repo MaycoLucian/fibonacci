c2c8311e338dc340acdea4f813a801750883899e
mayco <mayco@Mayco-Lucian>
2018-08-12 21:29:57 -0300

    ajustes

--- Aula1Fibonacci/src/br/com/team/calor/main/Recursao.java
+++ Aula1Fibonacci/src/br/com/team/calor/main/Recursao.java
@@ -9,10 +9,13 @@ public class Recursao {
 	public static void main(String[] args) {
 		
 		int numero = Integer.parseInt(JOptionPane.showInputDialog("Informe o valor"));
-		
-		long valorFibonacci = Fibonacci.fibo(numero);
-		
-		JOptionPane.showMessageDialog(null, "O valor do Fibonacci "+numero + " é: " + valorFibonacci);
+			
+		for (int i = 0; i < numero; i++) {
+            System.out.print("(" + i + "):" + Fibonacci.fibo(i) + "\t");
+        }
+  
+    	//long valorFibonacci = Fibonacci.fibo(numero);
+		//JOptionPane.showMessageDialog(null, "O valor do Fibonacci "+numero + " é: " + valorFibonacci);
 	}
 
 }
